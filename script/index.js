86c8bcc9f372a599f6ae6e2de427c80ca250e911
YuriLoureiro <loureirz@gmail.com>
2024-03-02 16:30:34 +0000

    index js

---
+++ script/index.js
@@ -0,0 +1,24 @@
+/*const profileButton = document.querySelector('.profile__info-button');
+const formEdit = document.querySelector('.popup');
+const formOpen = document.querySelector('.popup__opened');
+
+function openForm() {
+  if (formEdit.classList.contains('.popup')) {
+    formEdit.classList.remove('.popup');
+  } else {
+    formEdit.classList.add('.popup__opened');
+  }
+}
+
+profileButton.addEventListener("click", function(){
+
+  console.log("clicked!");
+});*/
+
+const profileButton = document.querySelector('.profile__info-button');
+const formEdit = document.querySelector('.popup');
+const formOpen = document.querySelector('.popup__opened');
+
+profileButton.addEventListener('click', function(){
+  if formEdit.classlist
+});
