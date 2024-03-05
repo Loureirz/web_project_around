e0dc32d69c287fdf6287bb46b9ec58fa4432b2c9
YuriLoureiro <loureirz@gmail.com>
2024-03-05 10:04:20 +0000

    correção js

--- script/index.js
+++ script/index.js
@@ -1,24 +1,27 @@
-/*const profileButton = document.querySelector('.profile__info-button');
-const formEdit = document.querySelector('.popup');
-const formOpen = document.querySelector('.popup__opened');
-
-function openForm() {
-  if (formEdit.classList.contains('.popup')) {
-    formEdit.classList.remove('.popup');
-  } else {
-    formEdit.classList.add('.popup__opened');
-  }
-}
-
-profileButton.addEventListener("click", function(){
-
-  console.log("clicked!");
-});*/
-
 const profileButton = document.querySelector('.profile__info-button');
 const formEdit = document.querySelector('.popup');
 const formOpen = document.querySelector('.popup__opened');
+const formClose = document.querySelector('.popup__close');
 
-profileButton.addEventListener('click', function(){
-  if formEdit.classlist
+const formElement = document.querySelector('.form__fieldset');
+const nameInput = document.querySelector('.form__name-input');
+const jobInput = document.querySelector('.form__about-input');
+const nameProfile = document.querySelector('.profile__info-name');
+const jobProfile = document.querySelector('.profile__info-text');
+
+profileButton.addEventListener("click", function(){
+  formOpen.style.display = 'flex';
 });
+
+formClose.addEventListener("click", function(){
+  formOpen.style.display = 'none';
+})
+
+formElement.addEventListener("submit", function(evt){
+  evt.preventDefault();
+
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+
+  formOpen.style.display = 'none';
+})
