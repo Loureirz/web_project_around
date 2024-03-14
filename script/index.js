81dab5b0a5491804a8a8557203f9d23bc8d5cdaa
YuriLoureiro <loureirz@gmail.com>
2024-03-14 10:13:59 +0000

    add popupcard

--- script/index.js
+++ script/index.js
@@ -1,3 +1,33 @@
+//INITIAL CARDS//
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+//POPUP PROFILE - Open, Close and Edit//
+
 const profileButton = document.querySelector('.profile__info-button');
 const formEdit = document.querySelector('.popup');
 const formOpen = document.querySelector('.popup-opened');
@@ -24,4 +54,18 @@ formElement.addEventListener("submit", function(evt){
   jobProfile.textContent = jobInput.value;
 
   formOpen.style.display = 'none';
+})
+
+//POPUP CARD - Open and Close//
+
+const cardButton = document.querySelector('.profile__button');
+const cardOpen = document.querySelector('.overlay__open');
+const cardClose = document.querySelector('.overlay__close');
+
+cardButton.addEventListener("click", function(){
+  cardOpen.style.display = 'flex';
+})
+
+cardClose.addEventListener("click", function(){
+  cardOpen.style.display = 'none';
 })
