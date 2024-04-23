8b9ee6e6e0405ccdac65393184ca9022d0e71a25
YuriLoureiro <loureirz@gmail.com>
2024-04-23 08:31:54 +0100

    refatoração finalizada

--- script/index.js
+++ script/index.js
@@ -1,8 +1,9 @@
-import { enableValidation } from './validate.js';
+import FormValidator from './FormValidator.js';
+import Card from './card.js';
 
 //INITIAL CARDS//
 
-const initialCards = [
+ const initialCards = [
   {
     name: "Vale de Yosemite",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
@@ -29,33 +30,24 @@ const initialCards = [
   }
 ];
 
-//POPUP PROFILE - Open, Close and Edit//
+//Constantes//
 
-const profileButton = document.querySelector('.profile__info-button');
-const formEdit = document.querySelector('.popup');
-const formOpen = document.querySelector('.popup-opened');
-const formClose = document.querySelector('.popup__close');
-const formModal = document.querySelector('.popup__container');
+export const profileButton = document.querySelector('.profile__info-button');
+export const formEdit = document.querySelector('.popup');
+export const formOpen = document.querySelector('.popup-opened');
+export const formClose = document.querySelector('.popup__close');
+export const formElement = document.querySelector('.form__fieldset');
+export const nameInput = document.querySelector('.form__input-name');
+export const jobInput = document.querySelector('.form__input-job');
+export const nameProfile = document.querySelector('.profile__info-name');
+export const jobProfile = document.querySelector('.profile__info-text');
+export const cardButton = document.querySelector('.profile__button');
+export const cardOpen = document.querySelector('.overlay-open');
+export const cardClose = document.querySelector('.overlay__close');
+export const cardOut = document.querySelector('.overlay');
+export const zoomOpen = document.querySelector('.zoom-open');
+export const zoom = document.querySelector('.zoom');
 
-const formElement = document.querySelector('.form__fieldset');
-const nameInput = document.querySelector('.form__input-name');
-const jobInput = document.querySelector('.form__input-job');
-const nameProfile = document.querySelector('.profile__info-name');
-const jobProfile = document.querySelector('.profile__info-text');
-
-profileButton.addEventListener("click", function(){
-  formOpen.style.display = 'flex';
-});
-
-formClose.addEventListener("click", function(){
-  formOpen.style.display = 'none';
-});
-
-formEdit.addEventListener("click", function(e) {
-  if(e.target == formEdit) {
-    formOpen.style.display = 'none';
-  }
-})
 
 formElement.addEventListener("submit", function(evt){
   evt.preventDefault();
@@ -66,150 +58,54 @@ formElement.addEventListener("submit", function(evt){
   formOpen.style.display = 'none';
 })
 
-//POPUP CARD - Open and Close//
-
-const cardButton = document.querySelector('.profile__button');
-const cardOpen = document.querySelector('.overlay-open');
-const cardClose = document.querySelector('.overlay__close');
-const cardOut = document.querySelector('.overlay');
-
-cardButton.addEventListener("click", function(){
-  cardOpen.style.display = 'flex';
-});
-
-cardClose.addEventListener("click", function(){
-  cardOpen.style.display = 'none';
-});
-
-cardOut.addEventListener("click", function(e) {
-  if(e.target == cardOut) {
-    cardOpen.style.display = 'none';
-  }
-});
-
-//POPUP CARD - Add image//
-
-function renderCard(card) {
-  const template = document
-    .querySelector("#template")
-    .content.querySelector(".elements__card");
-
-  const currentCard = template.cloneNode(true);
-
-  currentCard.querySelector(".elements__card-name").textContent = card.name;
-
-  currentCard
-    .querySelector(".elements__card-image")
-    .setAttribute("src", card.link);
-
-  currentCard
-    .querySelector(".elements__card-image")
-    .setAttribute("alt", card.name);
-
-  currentCard
-    .querySelector(".elements__card-image")
-    .addEventListener("click", (evt) => {
-      const zoomPopup = document.querySelector('.zoom__popup');
-      const zoomOpen = document.querySelector('.zoom-open');
-
-      const imageElement = document.querySelector('.zoom__popup-image');
-      const nameElement = document.querySelector('.zoom__popup-text');
-
-      imageElement.src = card.link;
-      imageElement.alt = card.name;
-      nameElement.textContent = card.name;
-
-      zoomOpen.style.display = 'flex';
-
-      const zoomClose = document.querySelector('.zoom__close');
-
-      zoomClose.addEventListener("click", function(){
-        zoomOpen.style.display = 'none';
-      })
-    })
-
-  currentCard
-    .querySelector(".elements__delete-icon")
-    .addEventListener("click", (evt) => {
-      const elements = document.querySelector(".elements");
-      const card = evt.target.offsetParent;
-
-      elements.removeChild(card);
-    });
-
-  currentCard
-    .querySelector(".elements__like-icon")
-    .addEventListener("click", (evt) => {
-      if (evt.target.getAttribute("src") === "./images/heart.svg") {
-        return evt.target.setAttribute(
-          "src",
-          "./images/like.svg"
-        );
-      }
-
-      return evt.target.setAttribute("src", "./images/heart.svg");
-    });
-
-  return currentCard;
-}
+// Gerador de cards //
 
 const elements = document.querySelector(".elements");
 
-initialCards.forEach((card, index) => {
-  const cardItem = renderCard(card);
-  elements.append(cardItem);
+initialCards.forEach((card) => {
+  const cardInstance = new Card(card, document.querySelector("#template"));
+  const generatedCard = cardInstance.generateCard();
+  elements.append(generatedCard);
 });
 
 const formAddCard = document.querySelector('.formcard__fieldset');
-formAddCard.addEventListener("submit", function(evt){
+
+formAddCard.addEventListener("submit", function(evt) {
   evt.preventDefault();
+
   const inputTitle = formAddCard.querySelector('.formcard__input-title');
   const inputImage = formAddCard.querySelector('.formcard__input-link');
 
-  const newCard = renderCard({
+  const newCard = new Card({
     name: inputTitle.value,
     link: inputImage.value,
   });
 
-  document.querySelector(".elements").prepend(newCard);
+  const generatedCard = newCard.generateCard();
+
+  document.querySelector(".elements").prepend(generatedCard);
+
   formAddCard.reset();
 
   cardOpen.style.display = 'none';
-})
-
-const zoomOpen = document.querySelector('.zoom-open');
-const zoom = document.querySelector('.zoom');
-
-zoom.addEventListener("click", function(e) {
-  if(e.target == zoom) {
-    zoomOpen.style.display = 'none';
-  }
 });
 
-function escEventListener(event) {
-  if (event.key === "Escape") {
-    formOpen.style.display = 'none';
-    cardOpen.style.display = 'none';
-    zoomOpen.style.display = 'none';
-  }
-}
+// Objetos //
 
-document.addEventListener('keydown', escEventListener);
-
-enableValidation({
-  formSelector: ".form__fieldset",
-  inputSelectorOne: ".form__input-name",
-  inputSelectorTwo: ".form__input-job",
-  submitButtonSelector: ".form__submit",
-  errorClassOne: ".form__input-name-error",
-  errorClassTwo: ".form__input-job-error",
-});
-
-enableValidation({
+new FormValidator({
   formSelector: ".formcard__fieldset",
   inputSelectorOne: ".formcard__input-title",
   inputSelectorTwo: ".formcard__input-link",
   submitButtonSelector: ".formcard__submit",
   errorClassOne: ".formcard__input-title-error",
   errorClassTwo: ".formcard__input-link-error",
-});
+}, formAddCard).enableValidation();
+
+new FormValidator({
+  formSelector: ".form__fieldset",
+  inputSelectorOne: ".form__input-name",
+  inputSelectorTwo: ".form__input-job",
+  submitButtonSelector: ".form__submit",
+  errorClassOne: ".form__input-name-error",
+  errorClassTwo: ".form__input-job-error",
+}, formElement).enableValidation();
